3362dd926c9ca85906823ec3ab18aa0934bb9304
Daniel Tan <dtch1997@stanford.edu>
2020-11-07 16:15:56 +0800

    Add DTSPMS to module import

--- problems/__init__.py
+++ problems/__init__.py
@@ -3,3 +3,4 @@ from problems.vrp.problem_vrp import CVRP, SDVRP
 from problems.op.problem_op import OP
 from problems.pctsp.problem_pctsp import PCTSPDet, PCTSPStoch
 from problems.dtspss.problem_dtspss import DTSPSS
+from problems.dtspms.problem_dtspms import DTSPMS
